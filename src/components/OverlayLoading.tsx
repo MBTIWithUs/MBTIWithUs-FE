76653753d005bba97e1aab09f93e66bc567b8420
ha4219 <ha4219@naver.com>
2022-10-02 04:28:52 +0900

    <FEAT> (FE): add loading component #1

---
+++ src/components/OverlayLoading.tsx
@@ -0,0 +1,21 @@
+import { Box, CircularProgress, Dialog, Modal } from '@mui/material';
+import React from 'react';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+};
+
+const OverlayLoading = ({ isLoading }: { isLoading: boolean }) => {
+  return (
+    <Modal open={isLoading}>
+      <Box sx={style}>
+        <CircularProgress size="5em" />
+      </Box>
+    </Modal>
+  );
+};
+
+export default OverlayLoading;
--- src/pages/Mbti.tsx
+++ src/pages/Mbti.tsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Container, Divider } from '@mui/material';
+import { Box, Button, Container, Divider, Typography } from '@mui/material';
 import RadioButtonsGroup from '@components/buttons/RadioButtonGroup';
 import { getQuestion } from '@mock/data';
 import { IQuestion } from 'types';
+import OverlayLoading from '@components/OverlayLoading';
 
 const MAX_QUESTIONS = 6;
 
 const MbtiPage = () => {
   const [page, setPage] = useState(0);
+  const [isLoading, setLoading] = useState(true);
   const [questions, setQuestions] = useState<IQuestion[]>([]);
 
   const getData = async () => {
     const data: IQuestion[] = await getQuestion();
 
     setQuestions(data);
+    setLoading(false);
   };
 
   const onNextClick = () => {
@@ -26,31 +29,43 @@ const MbtiPage = () => {
   }, []);
 
   return (
-    <Container maxWidth="md">
-      <Divider />
-      {questions
-        .slice(page * MAX_QUESTIONS, (page + 1) * MAX_QUESTIONS)
-        .map((item, index) => (
-          <RadioButtonsGroup
-            key={item.question}
-            index={page * MAX_QUESTIONS + index}
-            title={item.question}
-            leftQuestion={item.left_answer}
-            rightQuestion={item.right_answer}
-          />
-        ))}
-      <Divider />
-      <Box sx={{ textAlign: 'center' }} mt={2}>
-        {page < Math.floor(questions.length / MAX_QUESTIONS) - 1 ? (
-          <Button variant="contained" size="large" onClick={onNextClick}>
-            NEXT
-          </Button>
-        ) : (
-          <Button variant="contained" size="large">
-            SUBMIT
-          </Button>
-        )}
-      </Box>
+    <Container>
+      {isLoading ? (
+        <OverlayLoading isLoading />
+      ) : (
+        <Container maxWidth="md">
+          <Box>
+            <Typography variant="h5" fontWeight={900}>
+              성격 유형 퀴즈
+            </Typography>
+            <Typography variant="subtitle2">성격 유형 퀴즈</Typography>
+          </Box>
+          <Divider />
+          {questions
+            .slice(page * MAX_QUESTIONS, (page + 1) * MAX_QUESTIONS)
+            .map((item, index) => (
+              <RadioButtonsGroup
+                key={item.question}
+                index={page * MAX_QUESTIONS + index}
+                title={item.question}
+                leftQuestion={item.left_answer}
+                rightQuestion={item.right_answer}
+              />
+            ))}
+          <Divider />
+          <Box sx={{ textAlign: 'center' }} mt={2}>
+            {page < Math.floor(questions.length / MAX_QUESTIONS) - 1 ? (
+              <Button variant="contained" size="large" onClick={onNextClick}>
+                NEXT
+              </Button>
+            ) : (
+              <Button variant="contained" size="large">
+                SUBMIT
+              </Button>
+            )}
+          </Box>
+        </Container>
+      )}
     </Container>
   );
 };
