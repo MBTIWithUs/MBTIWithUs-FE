b663cce7d3e493a7069017d0b6f2483238969e9f
ha4219 <ha4219@naver.com>
2022-11-28 22:53:46 +0900

    <FEAT> (FE): user community #17

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,32 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import BoardListItem from '@components/board/BoardListItem';
 import OverlayLoading from '@components/OverlayLoading';
 import { Container, List, Typography } from '@mui/material';
 import { BoardListType } from 'features/board/types';
-import useSWR from 'swr';
+import useSWRInfinite, { SWRInfiniteKeyLoader } from 'swr/infinite';
 import { UserStateContext } from '@contexts/UserContext';
 import api from '@libs/api';
 import BoardWriter from '@components/board/BoardWriter';
 
 const isLoading = false;
+const LIMIT = 10;
 
 const BoardPage = () => {
   const auth = useContext(UserStateContext);
-  const { data } = useSWR<BoardListType>(
-    typeof window === 'undefined'
-      ? 'null'
-      : !auth?.token
-      ? '/api/v1/community/search/anonymous'
-      : '/api/v1/community/search',
-    (url) =>
-      api
-        .get(url, {
-          headers: {
-            Authorization: `Bearer ${auth?.token?.access_token}`,
-          },
-        })
-        .then((res) => res.data),
+  const [page, setPage] = useState(1);
+  // <BoardListType>
+
+  const getKey: SWRInfiniteKeyLoader = (pageIndex, previousPageData) => {
+    if (previousPageData && !previousPageData.length) return null;
+
+    return !auth?.token
+      ? `/api/v1/community/search/anonymous?page=${pageIndex}&limit=${LIMIT}`
+      : `/api/v1/community/search?page=${pageIndex}&limit=${LIMIT}`;
+  };
+
+  const { data } = useSWRInfinite(getKey, (url) =>
+    api
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${auth?.token?.access_token}`,
+        },
+      })
+      .then((res) => res.data),
   );
+
+  const articles = data ? data.map((item) => item.items).flat() : [];
+  console.log(articles);
 
   return (
     <Container sx={{ py: 3 }}>
@@ -44,10 +53,8 @@ const BoardPage = () => {
           </Typography>
           <BoardWriter tag="" />
           <List>
-            {data &&
-              data?.items.map((item) => (
-                <BoardListItem key={item.id} {...item} />
-              ))}
+            {/* {articles &&
+              articles.map((item) => <BoardListItem key={item.id} {...item} />)} */}
           </List>
         </Container>
       )}
