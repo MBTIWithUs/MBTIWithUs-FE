d735d5a80a8b7b2af7c796e7ddcfebc5774b4707
ha4219 <ha4219@naver.com>
2022-12-08 01:39:31 +0900

    <CHORE> (fe): test

--- src/pages/board/board.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import BoardPage from '.';
-
-test('test 시작하기', () => {
-  render(<BoardPage />);
-  const divEl = screen.getByText(/123/);
-  expect(divEl).toBeInTheDocument();
-});
